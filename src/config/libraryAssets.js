90ba7559bdf2a84dfa68ff78cac25650965857e4
Alex Pelan <alexpelan@gmail.com>
2020-05-10 23:06:37 -0400

    WIP: Lodash from npm

--- src/config/libraryAssets.js
+++ src/config/libraryAssets.js
@@ -2,13 +2,13 @@
 
 import BOOTSTRAP_CSS from '../../bower_components/bootstrap/dist/css/bootstrap.min.css';
 import BOOTSTRAP_JS from '../../bower_components/bootstrap/dist/js/bootstrap.min.js';
-import LODASH from '../../bower_components/lodash/dist/lodash.min.js';
 import MUSTACHE from '../../bower_components/mustache.js/mustache.js';
 import JQUERY from '../../node_modules/jquery/dist/jquery.min.js';
+import LODASH from '../../node_modules/lodash/lodash.min.js';
 import P5 from '../../node_modules/p5/lib/p5.min.js';
 
 export const jquery = {javascript: JQUERY};
-
+console.log("lodash is ", LODASH) // eslint-disable-line
 export const lodash = {javascript: LODASH};
 
 export const mustache = {javascript: MUSTACHE};
